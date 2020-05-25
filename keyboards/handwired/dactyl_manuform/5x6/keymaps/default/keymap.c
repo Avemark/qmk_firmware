e173820bb992c2f0e3e26d82d8c9ec466c6eb3c0
Christian Avemark <christian.avemark@klarna.com>
2020-05-25 13:48:43 +0200

    Use colon as shift and flesh out the special layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -18,13 +18,15 @@
 #define COLEMAK DF(_COLEMAK)
 #define QWERTY  DF(_QWERTY)
 
+#define SFT_CLN MT(RSFT, KC_CLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_COLEMAK] = LAYOUT_5x6(
      KC_ESC  , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0    ,KC_EQL,
      KC_SLASH, KC_Q  , KC_W  , KC_F  , KC_P  , KC_G  ,                         KC_J  , KC_L  , KC_U  , KC_Y  , KC_QUOT ,KC_NO,
-     KC_LSFT,  KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                         KC_H  , KC_N  , KC_E  , KC_I  , KC_O    ,KC_SCLN,
-     KC_LCTL , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_K  , KC_M  ,KC_COMM, KC_DOT, KC_MINS ,KC_RSFT,
+     KC_LSFT,  KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                         KC_H  , KC_N  , KC_E  , KC_I  , KC_O    ,SFT_CLN,
+     KC_LCTL , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_K  , KC_M  ,KC_COMM, KC_DOT, KC_MINS ,KC_RCTL,
                        KC_NO ,KC_LALT,                                                         KC_NO , KC_RCTL,
                                       KC_TAB, KC_BSPC,                         KC_SPC, KC_ENT,
                                       KC_LALT,NUMBERS,                         SPECIAL,KC_DEL,
@@ -44,8 +46,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_SPECIAL] = LAYOUT_5x6(
      _______,_______,_______,_______,_______,_______,                          _______,_______,_______,_______,_______,_______,
      _______,_______,_______,_______,_______,_______,                          _______,_______, KC_UP ,_______,_______,_______,
-     _______,_______,_______,_______,_______,_______,                          _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,
-     _______,_______,_______,_______,_______,_______,                          _______,_______,_______,_______,_______,_______,
+     _______,KC_LABK,KC_RABK,KC_LPRN,KC_RPRN,KC_DQUO,                          _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,
+     _______,_______,_______,KC_LCBR,KC_RCBR,KC_AT,                          _______,_______,_______,_______,_______,_______,
                      _______,_______,                                                          _______,_______,
                                      _______,_______,                          _______,_______,
                                      _______,_______,                          _______,_______,
