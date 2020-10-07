c09d453820bc8f8809911b69a3d7cc12b1b6b81c
Christian Avemark <christian.avemark@klarna.com>
2020-10-07 14:44:22 +0200

    tweak slash location

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/default/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_SLSH , KC_Q  , KC_W  , KC_F  , KC_P  , KC_G  ,                         KC_J  , KC_L  , KC_U  , KC_Y  , KC_QUOT ,KC_NO  ,
      KC_LSFT , KC_A  , KC_R  , KC_S  , KC_T  , KC_D  ,                         KC_H  , KC_N  , KC_E  , KC_I  , KC_O    ,KC_RSFT,
      KC_LCTL , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_K  , KC_M  ,KC_COMM, KC_DOT, KC_MINS ,KC_RCTL,
-                       KC_NO ,KC_LALT,                                                         KC_NO , KC_RCTL,
+                       KC_NO ,KC_LALT,                                                         KC_NO , KC_NO,
                                       KC_TAB, KC_BSPC,                         KC_SPC, KC_ENT,
                                       KC_LALT,NUMBERS,                         SPECIAL,KC_DEL,
                                       KC_LGUI, KC_NO ,                         MO(_RESET) , KC_NO
@@ -42,9 +42,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SPECIAL] = LAYOUT_5x6(
      TLAYOUT,_______,_______,_______,_______,_______,                          _______,_______,_______,_______ ,_______,_______,
-     _______,_______,_______,KC_LBRC,KC_RBRC,KC_GRV ,                          _______,_______, KC_UP ,KC_COLN ,_______,_______,
-     KC_BSLS,KC_LABK,KC_RABK,KC_LPRN,KC_RPRN,KC_DQUO,                          _______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,
-     _______,_______,_______,KC_LCBR,KC_RCBR,KC_AT  ,                          _______,_______,_______,_______ ,_______,_______,
+     _______,KC_SLSH,KC_BSLS,KC_LBRC,KC_RBRC,KC_GRV ,                          _______,KC_EQL , KC_UP ,_______ ,_______,_______,
+     KC_LSFT,KC_LABK,KC_RABK,KC_LPRN,KC_RPRN,KC_DQUO,                          _______,KC_LEFT,KC_DOWN,KC_RIGHT,KC_COLN,_______,
+     _______,_______,_______,KC_LCBR,KC_RCBR,KC_AT  ,                          _______,_______,_______,_______ ,KC_SCLN,_______,
                      _______,_______,                                                          _______,_______ ,
                                      _______,_______,                          _______,_______,
                                      _______,_______,                          _______,_______,
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      _______,_______,_______,_______,_______,_______,                          _______,KC_4   ,KC_5   ,KC_6   ,_______,_______,
      _______,_______,_______,_______,_______,_______,                          _______,KC_1   ,KC_2   ,KC_3   ,_______,_______,
                      _______,_______,                                                          _______,_______,
-                                     _______,_______,                          _______,_______,
+                                     _______,_______,                          KC_0   ,_______,
                                      _______,_______,                          _______,_______,
                                      _______,_______,                          _______,_______
   ),
